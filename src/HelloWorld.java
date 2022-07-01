a1d538970b21fdac95214d667ade5e8a38919d20
unknown <pcarmona5745@gmail.com>
2022-06-30 20:55:17 -0600

    second commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world!");
+		int num1 = 2;
+		int num2 = 5;
+		int num3 = 7;
+		if((num1 > num2) && (num2 < num3) && (num1 > num3)){
+	        System.out.println(true);
+	        } else {
+	            System.out.println(false);
+	        }
 
 	}
 
